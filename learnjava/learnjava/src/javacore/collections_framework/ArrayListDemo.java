9774ef06270a62dffd31e83e23db5eb9d1232065
aayushmishra17 <m.aayush.k@gmail.com>
2025-01-10 23:02:57 +0530

    Added more methods in ArrayList file.

--- learnjava/learnjava/src/javacore/collections_framework/ArrayListDemo.java
+++ learnjava/learnjava/src/javacore/collections_framework/ArrayListDemo.java
@@ -22,24 +22,34 @@ public class ArrayListDemo {
                 new ArrayList<>(fixed size or unmodifiable list);
          */
 
-        list.add(77);
+        list.add(77); // add an element at the end of list. Avg: O(1), Worst: O(N) when array resizing is needed.
+        list.add(3, 12); // add element at specific index. O(N)
+        list.addAll(List.of(111,112,113,114,115));
+        list.addAll(7, List.of(111,112,113,114,115));
 
-        list.get(0);
-        list.size();
+        list.get(0); // get element at specific index. O(1)
+        list.size(); // number of elements currently present in the list.
+        list.indexOf(5); // returns -1 if element not present in list.
+        list.lastIndexOf(5);
+        list.isEmpty(); // check if list contains any elements
 
         list.contains(5);
+        list.containsAll(List.of(33,43,53));
 
         list.remove(2);
         list.remove(Integer.valueOf(7));
 
-        list.add(3, 12);
-        list.addAll(List.of(111,112,113,114,115));
-        list.set(3, 69);
+        list.replaceAll(num -> num * 2);
+        list.retainAll(List.of(1,2,3,4,5)); // remove all elements that are not present in specific collection.
+
+        list.set(3, 69); // modify an element at specific index.
 
         list.trimToSize(); // trims the internal array's size to the current size.
 
         // List to array
         Integer[] array = list.toArray(new Integer[0]);
+
+        list.subList(5, 10).clear(); // returns a SubList view of the list b/t [inclusiveIndx, exclusiveIndx), changes made to the sublist is visible in the actual list.
 
         // Sort an arraylist
         Collections.sort(list);
@@ -49,6 +59,8 @@ public class ArrayListDemo {
         // We need to implement custom Comparator when a user defined type doesn't implement Comparable interface.
         list.sort((o1, o2) -> o1 - o2); // ascending order.
         list.sort((o1, o2) -> o2 - o1); // descending order.
+
+        list.clear(); // empty a list.
 
         class Student {
 
